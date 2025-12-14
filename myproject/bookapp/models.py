ab35e437915a09b4a18e5beec59ee68bf0805779
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-12-14 20:15:16 +0900

    Migrateの実行

--- myproject/bookapp/models.py
+++ myproject/bookapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Bookモデルの定義
+class Book(models.Model):
+    # 書籍のタイトル(最大20文字)
+    title = models.CharField(max_length=20)
+
+    # 著名者(最大10文字)
+    author = models.CharField(max_length=10)
+
+    # 出版日
+    publication_date = models.DateField()
+
+    # オブジェクトの文字列表現を定義
+    def __str__(self):
+        # 書籍のタイトルを返す。
+        return self.title
