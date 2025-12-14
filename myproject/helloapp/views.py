fe1e41a747b0ee6aa0d064eaae7148b0058a1f9a
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-12-14 17:44:26 +0900

    アプリケーションviews.pyの修正
    稼働確認
    PART 1の学習完了

--- myproject/helloapp/views.py
+++ myproject/helloapp/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# --------------------------------------------------------
+# ビュー関数を定義
+# "ハロー、Django!"というテキストを含むHTTPレスポンスを返す。
+# --------------------------------------------------------
+def show_hello(request):
+    return HttpResponse("ハロー、Django!")
