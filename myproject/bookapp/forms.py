268f9ad64723f15351c3b294ecd002acd7df6ac8
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-12-18 20:50:00 +0900

    5-2 入力チェック(バリデーション)の実装の学習完了

--- myproject/bookapp/forms.py
+++ myproject/bookapp/forms.py
@@ -22,3 +22,31 @@ class BookForm(forms.ModelForm):
         # △△△△ 4.13 △△△△
         # △△△△ 4.11 △△△△
         widgets = {"publication_date": forms.DateInput(attrs={"type": "date"})}
+
+        error_messages = {
+            "title": {
+                "max_length": "「タイトル」は20文字以内で入力してください！",
+                "required": "「タイトル」を入力してください！",
+            },
+            "author": {
+                "max_length": "「著者名」は10文字以内で入力してください！",
+                "required": "「著者名」を入力してください！",
+            },
+        }
+
+    # タイトルに対するバリデーション
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if "盗作" in title:  # type: ignore
+            raise forms.ValidationError("タイトルに'盗作'を含めることはできません。")
+        return title
+
+    # フォーム全体を対象にしたバリデーション
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get("title")
+        author = cleaned_data.get("author")
+
+        if title == author:
+            raise forms.ValidationError("タイトルと著名者は同じにできません。")
+        return cleaned_data
