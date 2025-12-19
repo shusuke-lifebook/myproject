98886090d9ebfd9e7e298574298260f5cb5e68ac
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-12-19 21:53:14 +0900

    6-3 Django管理画面のカスタマイズの学習完了

--- myproject/bookapp/admin.py
+++ myproject/bookapp/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Book
 
+
+# Bookモデルの管理画面をカスタマイズするクラス
+class BookAdmin(admin.ModelAdmin):
+    # 表示するフィールド
+    list_display = ("title", "author", "publication_date")
+
+    # 検索フィールドを指定
+    search_fields = ("title", "author")
+
+    # フィルタリングするフィールドを指定
+    list_filter = ("publication_date",)
+
+
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
--- myproject/bookapp/models.py
+++ myproject/bookapp/models.py
@@ -6,17 +6,19 @@ from django.db import models
 # Bookモデルの定義
 class Book(models.Model):
     # 書籍のタイトル(最大20文字)
-    title = models.CharField(max_length=20)
+    title = models.CharField(verbose_name="タイトル", max_length=20)
 
     # 著名者(最大10文字)
-    author = models.CharField(max_length=10)
+    author = models.CharField(verbose_name="著者", max_length=10)
 
     # 出版日
-    publication_date = models.DateField()
+    publication_date = models.DateField(verbose_name="出版日")
 
     # ▽▽▽▽ 4.12 ▽▽▽▽
     # 画像フィールドを追加
-    cover_image = models.ImageField(upload_to="book_covers/", blank=True, null=True)
+    cover_image = models.ImageField(
+        verbose_name="表紙画像", upload_to="book_covers/", blank=True, null=True
+    )
     # △△△△ 4.12 △△△△
 
     # オブジェクトの文字列表現を定義
