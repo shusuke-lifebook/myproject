a95e711dca3f538c3e0fe8f5d130d861620448cf
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-12-20 20:46:12 +0900

    詳細画面の実装

--- myproject/todoapp/urls.py
+++ myproject/todoapp/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     # 'list'というURLにアクセスすると、views.pyのTodoListViewクラスが呼び出される。
     # name='todo_list'は、このURLパターンに名前を付けている。
-    path("list/", views.TodoListView.as_view(), name="todo_list")
+    path("list/", views.TodoListView.as_view(), name="todo_list"),
+    # '<int:pk>/'というURLにアクセスすると、views.pyのTodoDetailViewクラスが呼び出される
+    # '<int:pk>はURLの一部として渡される整数の主キーを意味し、その主キーに基づいて特定のTodoを表示する。
+    # name='todo_detail'は、このURLパターンに名前を付けている。
+    path("<int:pk>/", views.TodoDetailView.as_view(), name="todo_detail"),
 ]
--- myproject/todoapp/views.py
+++ myproject/todoapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from . import models
 
 
 # Create your views here.
+# ========================================================
+# Todo一覧表示用のビュー： 継承元はListView
+# ========================================================
 class TodoListView(ListView):
     # 使用するモデルを指定(Todoモデル)
     model = models.Todo
@@ -12,3 +15,15 @@ class TodoListView(ListView):
     template_name = "todoapp/todo_list.html"
     # テンプレートで使用するオブジェクトリストの名前を指定
     context_object_name = "todos"
+
+
+# ========================================================
+# Todo詳細表示用のビュー： 継承元はDetailView
+# ========================================================
+class TodoDetailView(DetailView):
+    # 使用するモデルを指定(Todoモデル)
+    model = models.Todo
+    # 使用するテンプレートファイルを指定
+    template_name = "todoapp/todo_detail.html"
+    # テンプレートで使用するオブジェクトの名前を指定
+    context_object_name = "todo"
