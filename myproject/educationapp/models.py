8228ac83aa987906808f64ed0666cde33fa1e3fa
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-12-28 19:12:55 +0900

    Studentモデルの作成 マイグレーションファイルの作成

--- myproject/educationapp/models.py
+++ myproject/educationapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+# ===================================================
+# 生徒(Student) テーブル
+# ===================================================
+class Student(models.Model):
+    # 生徒の名前
+    name = models.CharField(max_length=100)
+    # 生徒の年齢
+    age = models.IntegerField()
+    # 入学日
+    enrollment_date = models.DateField()
