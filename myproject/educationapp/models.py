50342e3cb643b20ff25a15bb7a757cc141566b5f
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-01-01 14:48:11 +0900

    11-4 データ構造の変更の学習開始
    リスト11-17の学習完了。migrateの実行

--- myproject/educationapp/models.py
+++ myproject/educationapp/models.py
@@ -13,3 +13,19 @@ class Student(models.Model):
     age = models.IntegerField()
     # 入学日
     enrollment_date = models.DateField()
+
+
+# ===================================================
+# プロフィール(Profile) テーブル
+# ===================================================
+class Profile(models.Model):
+    # 生徒と1対1の関係を持つ
+    # OneToOneFieldを使用し、生徒とプロフィールが1対1の関係を保つ
+    # on_delete=models.CASCADEにより、生徒が削除された場合に対応するプロフィールも削除される
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    # 住所
+    address = models.CharField(max_length=255)
+    # 電話番号
+    phone_number = models.CharField(max_length=20)
+    # その他の情報
+    bio = models.TextField(null=True, blank=True)
