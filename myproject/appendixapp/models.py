31a8f7add2294d34a2d54f006ddcd4b0676dd0ae
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-01-02 14:46:19 +0900

    appendixappのSapmleを作成

--- myproject/appendixapp/models.py
+++ myproject/appendixapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+# データベースのテーブルに相当するモデルを定義
+class Sample(models.Model):
+    # 名前を保持するフィールド
+    name = models.CharField(max_length=100)
